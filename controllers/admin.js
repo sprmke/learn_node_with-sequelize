fc1267d3eec546f1eb31bfc919e0fa0cd39fab3e
Michael Manlulu <michael.manlulu@myresman.com>
2022-08-26 07:29:54 +0800

    158. fetching admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -66,11 +66,13 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products',
-    });
-  });
+  Product.findAll()
+    .then((products) => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
+    })
+    .catch((error) => console.log(error));
 };
