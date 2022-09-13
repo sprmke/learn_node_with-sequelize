6d37dd674f84fd01ea0e81f0f4b64306930a919c
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-14 03:55:13 +0800

    11.171 adding an order model

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 // Database
 const sequelize = require('./util/database');
@@ -40,6 +42,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorConroller.get404);
 
+// Model Associations
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
@@ -48,6 +51,10 @@ Cart.belongsTo(User);
 
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
   // .sync({ force: true })
---
+++ models/order.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Order = sequelize.define('order', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    unique: true,
+  },
+});
+
+module.exports = Order;
