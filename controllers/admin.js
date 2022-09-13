ac955828c80cf664ad8a11a591e7dc1729ed45a7
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-13 23:10:20 +0800

    11.164 using magic association methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title,
-    price,
-    imageUrl,
-    description,
-  })
+
+  req.user
+    .createProduct({
+      title,
+      price,
+      imageUrl,
+      description,
+    })
     .then((result) => {
       console.log(result);
       res.redirect('/admin/products');
