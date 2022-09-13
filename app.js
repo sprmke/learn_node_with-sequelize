6c99bf591ef28bfa9c355e3534f99c501926ae35
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-14 00:17:18 +0800

    11. 166 one-to-many & many-to-many relations

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const errorConroller = require('./controllers/error');
 // Models
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 // Database
 const sequelize = require('./util/database');
@@ -40,6 +42,12 @@ app.use(errorConroller.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync()
